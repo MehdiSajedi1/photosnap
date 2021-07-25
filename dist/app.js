a36c135e325d2d81548be54f83a76da0b2d0d7d6
Mehdi Sajedi <mehdiisajedi@gmail.com>
2021-07-24 21:24:42 -0700

    Finished mobile home page

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,11 @@
+history.scrollRestoration = 'auto';
+
 const menuBtn = document.querySelector('#menu-btn');
 const menu = document.querySelector('#menu');
-const overlay = document.querySelector('.overlay');
 
 menuBtn.addEventListener('click', (e) => {
   Array.from(e.target.closest('#menu-btn').children).forEach((icon) => {
     icon.classList.toggle('hidden');
   });
   menu.classList.toggle('show');
-  overlay.classList.toggle('hidden');
 });
