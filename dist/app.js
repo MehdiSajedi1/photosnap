f7e6a6fd972907dd5e2444342099a36cd57f0ac6
Mehdi Sajedi <mehdiisajedi@gmail.com>
2021-07-31 22:06:49 -0700

    Finished pricing page toggle functionality

--- dist/app.js
+++ dist/app.js
@@ -9,3 +9,32 @@ menuBtn.addEventListener('click', (e) => {
   });
   menu.classList.toggle('show');
 });
+
+const toggleBtn = document.querySelector('.toggle-btn');
+const priceFreqs = document.querySelectorAll('.price-freq');
+const priceAmounts = document.querySelectorAll('.price h1 span');
+const pricePer = document.querySelectorAll('.price p');
+
+let monthly = true;
+
+toggleBtn.addEventListener('click', (e) => {
+  e.target.closest('.toggle-btn').classList.toggle('switch');
+  priceFreqs.forEach((item) => item.classList.toggle('inactive'));
+
+  priceAmounts.forEach((amount) => {
+    if (monthly) {
+      amount.innerHTML = (+amount.innerHTML * 10).toFixed(2);
+    } else {
+      amount.innerHTML = (+amount.innerHTML / 10).toFixed(2);
+    }
+  });
+
+  pricePer.forEach((per) => {
+    if (monthly) {
+      per.innerHTML = 'per year';
+    } else {
+      per.innerHTML = 'per month';
+    }
+  });
+  monthly = !monthly;
+});
